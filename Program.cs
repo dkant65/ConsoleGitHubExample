02346fc9ff6c97d089f24ea96ffd2627553e8016
Damir Kantardzic <dkantardzic@bastiansolutions.com>
2020-05-06 14:16:58 -0400

    Change is made

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleGitHubExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Here is a change");
         }
     }
 }
